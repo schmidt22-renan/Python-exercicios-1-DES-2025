3d80214bc6c7465b9d255c6d25dc9b717807d368
schmidt22-renan <schmidt.renan@escola.pr.gov.br>
2025-06-03 12:19:01 +0000

    sala

--- exercicio02.py
+++ exercicio02.py
@@ -1,5 +1,14 @@
 #  Juliana está coordenando entregas de projetos e precisa calcular o tempo total necessário 
 # para finalizar três tarefas: X, Y e Z.
-#  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
+#  Se alguma tarefa estiver om o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
 
+Dia_01= int(input("Dia para atividade 01"))
+Dia_02 = int(input("Dia para atividade 02"))
+ Dia_03= int(input("Dia para atividade 03"))
+
+    if Dia_01<0 or Dia_02< o or Dia_03<0
+    print("erro numeros negativos")
+    else:
+    soma= Dia_01+ Dia_02+ Dia_03
+print(f"tempo total do projeto:{soma} dias")
