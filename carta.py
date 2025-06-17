97c5eea0596075391f609bfda4a1f8e4178c0f49
schmidt22-renan <schmidt.renan@escola.pr.gov.br>
2025-06-17 12:20:29 +0000

    salve carta

---
+++ carta.py
@@ -0,0 +1,7 @@
+destinatario = input("digite o  E-mail do destinatario: ") 
+mensagem = input("digite sua mensagem: ")
+remetente = input("digite seu nome: ")
+data = input("digite a data: ")
+
+print("\n seu E_mail")
+print(f"ola{destinatario}")
